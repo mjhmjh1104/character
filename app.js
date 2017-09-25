f01588cd0c5cb45c0713ec513d5916af8ae1053a
mjhmjh1104 <mjhmjh1104@naver.com>
2017-09-25 23:16:04 +0900

    updated

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+  res.render('main');
+});
+
+app.get('/ch/:data', function(req, res) {
+  res.render(req.params.data);
+});
+
+app.use(function(err,req,res,next) {
+  res.render('404');
+});
+
+app.listen(3000, function() {
+  console.log('Server Started');
+});
